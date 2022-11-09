d63868ebe72cf586b2192e0a91d7e7d38bf4585b
Kevin Li <kevin.li20021106@gmail.com>
2022-11-09 14:38:41 -0500

    update

---
+++ create_db.py
@@ -0,0 +1,13 @@
+# create_db.py
+
+
+from project.app import app, db
+from project.models import Post
+
+
+# create the database and the db table
+with app.app_context():
+    db.create_all()
+
+    # commit the changes
+    db.session.commit()
